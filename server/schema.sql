98c068331cfa1266e5a53a1aa5874b9ab601a787
jastra4 <josephstrandmo@gmail.com>
2017-12-15 16:32:16 -0600

    built out database structure

--- server/schema.sql
+++ server/schema.sql
@@ -1,17 +1,74 @@
-CREATE DATABASE chat;
 
-USE chat;
+-- ---
+-- Globals
+-- ---
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+-- SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+-- SET FOREIGN_KEY_CHECKS=0;
+
+-- ---
+-- Table 'users'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `users`;
+    
+CREATE TABLE `users` (
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `name` MEDIUMTEXT NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
 );
 
-/* Create other tables and define schemas for them here! */
+-- ---
+-- Table 'rooms'
+-- 
+-- ---
 
+DROP TABLE IF EXISTS `rooms`;
+    
+CREATE TABLE `rooms` (
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `name` MEDIUMTEXT NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
 
+-- ---
+-- Table 'messages'
+-- 
+-- ---
 
+DROP TABLE IF EXISTS `messages`;
+    
+CREATE TABLE `messages` (
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `message` MEDIUMTEXT NULL DEFAULT NULL,
+  `id_rooms` INTEGER NULL DEFAULT NULL,
+  `id_users` INTEGER NULL DEFAULT NULL,
+  UNIQUE KEY (`id`)
+);
 
-/*  Execute this file from the command line by typing:
- *    mysql -u root < server/schema.sql
- *  to create the database and the tables.*/
+-- ---
+-- Foreign Keys 
+-- ---
 
+ALTER TABLE `messages` ADD FOREIGN KEY (id_rooms) REFERENCES `rooms` (`id`);
+ALTER TABLE `messages` ADD FOREIGN KEY (id_users) REFERENCES `users` (`id`);
+
+-- ---
+-- Table Properties
+-- ---
+
+-- ALTER TABLE `users` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `rooms` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `messages` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ---
+-- Test Data
+-- ---
+
+-- INSERT INTO `users` (`id`,`name`) VALUES
+-- ('','');
+-- INSERT INTO `rooms` (`id`,`name`) VALUES
+-- ('','');
+-- INSERT INTO `messages` (`id`,`message`,`id_rooms`,`id_users`) VALUES
+-- ('','','','');
